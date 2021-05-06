d92f69fb05e9380f1ca545dc8391c965703daa25
FelipeCabreira <felipe.cabreira@edu.pucrs.br>
2021-05-05 23:38:05 -0300

    docs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+/*
+*  DOCS:
+*  Following this tutorial https://reactjs.org/tutorial/tutorial.html
+* */
+
+
 
 function Square(props) {
 
